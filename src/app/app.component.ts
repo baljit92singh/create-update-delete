bc53dd52b7f993429337316d7c77b7ec3d977160
Baljit Singh <balu.singh55@gmail.com>
2018-10-14 13:40:33 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+// import { ContactRequest } from '../app/app.module'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'create-update-delete';
+  contactForm: FormGroup;
+  recentList = [];
+  mode: string = 'new';
+  constructor(private fb: FormBuilder,
+    public snackBar: MatSnackBar) {
+    this.loadForm();
+  }
+  loadForm() {
+    this.contactForm = this.fb.group({
+      name: '',
+      email: '',
+      contactNumber: ''
+    })
+  }
+
+  saveContact() {
+    let item = {
+      name: this.contactForm.controls['name'].value,
+      email: this.contactForm.controls['email'].value,
+      contactNumber: this.contactForm.controls['contactNumber'].value
+    }
+    this.recentList.push(item);
+    this.resetForm();
+    var message = "Contact created successfully"
+    this.snackBar.open(message, "Dismiss", {
+      duration: 2000,
+    });
+  }
+
+  updateContact() {
+    this.recentList.splice(this.indexUpdate, 1);
+    let item = {
+      name: this.contactForm.controls['name'].value,
+      email: this.contactForm.controls['email'].value,
+      contactNumber: this.contactForm.controls['contactNumber'].value
+    }
+    this.recentList.push(item);
+    this.resetForm();
+    var message = "Contact update successfully"
+    this.snackBar.open(message, "Dismiss", {
+      duration: 2000,
+    });
+    this.mode = 'new';
+  }
+  resetForm() {
+    this.contactForm.reset();
+  }
+
+  indexUpdate: number;
+  editContact(item) {
+    this.mode = 'edit';
+    this.indexUpdate = this.recentList.indexOf(item)
+    // console.log(item);
+    this.contactForm.controls['name'].setValue(item.name);
+    this.contactForm.controls['email'].setValue(item.email);
+    this.contactForm.controls['contactNumber'].setValue(item.contactNumber);
+  }
+
+  deleteContact(item) {
+    // console.log(item);
+    this.recentList.splice(this.recentList.indexOf(item), 1);
+    var message = "Contact delete successfully"
+    this.snackBar.open(message, "Dismiss", {
+      duration: 2000,
+    });
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
